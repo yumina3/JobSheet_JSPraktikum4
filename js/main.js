14f8f45313b995d668a8874168e50611ce034693
yumina3 <yunamikanda06@gmail.com>
2026-09-15 14:26:15 +0700

    bagian 22 yach

--- js/main.js
+++ js/main.js
@@ -57,3 +57,19 @@ sortSelect.addEventListener("change", (event) => {
 state.products = dummyProducts;
 
 render();
+
+//BAGIAN 22
+const promise = new Promise((resolve, reject) => {
+  const success = true;
+
+  if (success) {
+    resolve("Data berhasil diambil");
+  } else {
+    reject("Terjadi error");
+  }
+});
+
+promise
+  .then(result => console.log(result))
+  .catch(error => console.error(error))
+  .finally(() => console.log("Selesai, apa pun hasilnya"));
